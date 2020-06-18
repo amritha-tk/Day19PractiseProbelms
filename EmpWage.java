4ecb3598400bd8abd1fa2efa8a07c826bd139515
amritha-tk <amrithatk95@gmail.com>
2020-06-18 18:26:08 +0530

    Added UC11

--- EmpWage.java
+++ EmpWage.java
@@ -1,6 +1,10 @@
 import java.lang.*;
 import java.util.*;
-  public class EmpWage {
+
+   interface WageCalculation{
+   void EmpWageCalc(String Company,int empRatePerHr,int workingDays,int empHrs);
+}
+class EmpWage implements WageCalculation {
    int a;
    int totalWhr=0;
 
@@ -12,17 +16,31 @@ import java.util.*;
         System.out.println("Today Absent");
    }
 
-   public EmpWage(String Company,int empRatePerHr,int workingDays,int empHrs){
+   public void EmpWageCalc(String Company,int empRatePerHr,int workingDays,int empHrs){
     totalWhr=empHrs*empRatePerHr;
     int monthlySalary=totalWhr*workingDays;
     System.out.println("For company " +Company+ " TotalWorking hr is "+totalWhr+" and Salary is " +monthlySalary);
-   }
+    }
+
 
    public static void main(String args[]){
 
      int empHrs=0;
      System.out.println("Welcome to Employee wage computation program");
      Scanner in = new Scanner(System.in);
+
+     //System.out.println("Enter the number of companies");
+     //int num=in.nextInt();
+     //for(int i=0;i<=num;i++)
+     //{
+      //System.out.println("Enter the company name");
+      //String company=in.next();
+
+      //System.out.println("Enter the emp rate per hour");
+      //int empRatePerHr=in.nextInt();
+
+      //System.out.println("Enter the number of working days");
+      //int workingDays=in.nextInt();
 
      System.out.println("For whom 1.ParttimeEmployee 2.FullTimeEmployee");
      int choice=in.nextInt();
@@ -38,13 +56,14 @@ import java.util.*;
                  break;
             }
 
-      EmpWage[] obj=new EmpWage[4];
-      obj[0]=new EmpWage("Reliance",21,20,empHrs);
-      obj[1]=new EmpWage("Flipkart",20,23,empHrs);
-      obj[2]=new EmpWage("Amazon",21,30,empHrs);
-      obj[3]=new EmpWage("Quest",20,14,empHrs);
-
+      EmpWage obj[]=new EmpWage[3];
+   
+       obj[0]=new EmpWage();
+       obj[0].EmpWageCalc("Reliance",20,30,empHrs);
+       obj[1]=new EmpWage();
+       obj[1].EmpWageCalc("Quest",20,25,empHrs);
+       obj[2]=new EmpWage();
+       obj[2].EmpWageCalc("Amazon",23,25,empHrs);
 }
 }
-
 
