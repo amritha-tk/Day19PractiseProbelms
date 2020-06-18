b2c71566d0ff97756ad7395c462e79c56b6adddf
amritha-tk <amrithatk95@gmail.com>
2020-06-18 22:03:36 +0530

    Added UC12

--- EmpWage.java
+++ EmpWage.java
@@ -1,46 +1,69 @@
 import java.lang.*;
 import java.util.*;
 
-   interface WageCalculation{
-   void EmpWageCalc(String Company,int empRatePerHr,int workingDays,int empHrs);
-}
-class EmpWage implements WageCalculation {
+public class EmpWage {
    int a;
    int totalWhr=0;
+   String company;
+   int empRatePerHr;
+   int workingDays;
+   int empHrs;
+   int monthlySalary;
 
-  public  void presence(){
-   a=(int)(Math.random()*2 +1);
-   if(a==1)
-        System.out.println("Present");
-   else
-        System.out.println("Today Absent");
+  public EmpWage(String company,int empRatePerHr,int workingDays,int empHrs){
+   this.company=company;
+   this.empRatePerHr=empRatePerHr;
+   this.workingDays=workingDays;
+   this.empHrs=empHrs;
    }
 
-   public void EmpWageCalc(String Company,int empRatePerHr,int workingDays,int empHrs){
-    totalWhr=empHrs*empRatePerHr;
-    int monthlySalary=totalWhr*workingDays;
-    System.out.println("For company " +Company+ " TotalWorking hr is "+totalWhr+" and Salary is " +monthlySalary);
+   //getmethods
+   public String getCompany(){
+    return company;
+   }
+   public int getempRatePerHr(){
+    return empRatePerHr;
+   }
+   public int getworkingDays(){
+    return workingDays;
+   }
+   public int getempHrs(){
+    return empHrs;
     }
 
+    //Setmethods
+   public void setCompany(String company){
+     this.company=company;
+    }
+    public void setempRatePerHr(int empRatePerHr){
+     this.empRatePerHr=empRatePerHr;
+    }
+    public void setworkingDays(int workingDays){
+     this.workingDays=workingDays;
+    }
+    public void setempHrs(int empHrs){
+     this.empHrs=empHrs;
+    }
 
    public static void main(String args[]){
 
      int empHrs=0;
+     List<EmpWage> empList=new ArrayList<EmpWage>(); 
      System.out.println("Welcome to Employee wage computation program");
      Scanner in = new Scanner(System.in);
 
-     //System.out.println("Enter the number of companies");
-     //int num=in.nextInt();
-     //for(int i=0;i<=num;i++)
-     //{
-      //System.out.println("Enter the company name");
-      //String company=in.next();
+     System.out.println("Enter the number of companies");
+     int num=in.nextInt();
+     for(int i=0;i<num;i++)
+     {
+      System.out.println("Enter the company name");
+      String company=in.next();
 
-      //System.out.println("Enter the emp rate per hour");
-      //int empRatePerHr=in.nextInt();
+      System.out.println("Enter the emp rate per hour");
+      int empRatePerHr=in.nextInt();
 
-      //System.out.println("Enter the number of working days");
-      //int workingDays=in.nextInt();
+      System.out.println("Enter the number of working days");
+      int workingDays=in.nextInt();
 
      System.out.println("For whom 1.ParttimeEmployee 2.FullTimeEmployee");
      int choice=in.nextInt();
@@ -55,15 +78,14 @@ class EmpWage implements WageCalculation {
         default:System.out.println("Nothing");
                  break;
             }
+      empList.add(new EmpWage(company,empRatePerHr,workingDays,empHrs));//totalWhr,monthlySalary));
+      for (EmpWage emp: empList){
+        int totalWhr=(emp.getempHrs()*emp.getempRatePerHr());
+        int monthlySalary=(totalWhr*emp.getworkingDays());
 
-      EmpWage obj[]=new EmpWage[3];
-   
-       obj[0]=new EmpWage();
-       obj[0].EmpWageCalc("Reliance",20,30,empHrs);
-       obj[1]=new EmpWage();
-       obj[1].EmpWageCalc("Quest",20,25,empHrs);
-       obj[2]=new EmpWage();
-       obj[2].EmpWageCalc("Amazon",23,25,empHrs);
-}
+         System.out.println("For company " +emp.getCompany()+ " TotalWorking hr is "+totalWhr+" and Salary is " +monthlySalary);
+      }
+  }
+ }
 }
 
